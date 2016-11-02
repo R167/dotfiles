9157675ad4dab096380699d177e0d76d0f21dbdf
Winston Durand <me@winstondurand.com>
2016-11-01 18:14:23 -0700

    fixed readline config

--- system/env.zsh
+++ system/env.zsh
@@ -1,3 +1,4 @@
 export EDITOR="vi"
 export ARCHFLAGS=-Wno-error=unused-command-line-argument-hard-error-in-future
 export RBENV_ROOT="$HOME/.rbenv"
+export RUBY_CONFIGURE_OPTS=--with-readline-dir=`brew --prefix readline`
