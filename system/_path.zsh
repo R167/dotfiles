46d5045e71acc949c32baf5484cdd065b833d49e
Winston Durand <me@winstondurand.com>
2017-01-26 15:55:28 -0800

    updated path var

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,3 +1,4 @@
-export PATH="~/dev/local/bin:~/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH:/usr/local/opt/coreutils/libexec/gnubin"
-export MANPATH="~/dev/local/man:/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+export GOPATH="$HOME/gopath"
+export PATH="$HOME/dev/local/bin:~/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$GOPATH:$GOPATH/bin:$ZSH/bin:$PATH:/usr/local/opt/coreutils/libexec/gnubin"
+export MANPATH="$HOME/dev/local/man:/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$GOPATH/man:$MANPATH"
 export NODE_PATH="/usr/local/lib/node_modules:$NODE_PATH"
