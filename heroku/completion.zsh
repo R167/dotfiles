ccba22483a7cec6a52bc0c19411b4366927a4f6c
Winston Durand <me@winstondurand.com>
2019-09-20 11:25:58 -0700

    Update dotfiles

---
+++ heroku/completion.zsh
@@ -0,0 +1,5 @@
+# heroku autocomplete setup
+HEROKU_AC_ZSH_SETUP_PATH=/Users/wmd/Library/Caches/heroku/autocomplete/zsh_setup
+
+test -f $HEROKU_AC_ZSH_SETUP_PATH && \
+    source $HEROKU_AC_ZSH_SETUP_PATH
